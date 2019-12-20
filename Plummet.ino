6aaf143efa5068799f688a3d07d2ad62722daabf
Adi Sharabani <adishar@gmail.com>
2019-12-19 20:56:20 -0800

    Attempt for more accurate Sync3

--- Plummet.ino
+++ Plummet.ino
@@ -1273,7 +1273,10 @@ void updateAmpAndTimeForSyncedRunning() {
 		} else {
 			tPhase = min(max(0.5 + ropeAngleOffset*ML_ROPE_OFFSET_TO_PHASE/(float(offset)/loopTime),0.25),0.75);
 			// dont decrease amp if long way from the right offset (and rope is not too (+0.1) big;
-mo
+/* 			if ((offset > loopTime*0.1) && (abs(ropeAngleOffset) < 0.1)) {
+				tPhase = 0.5+ML_EPS; // eps to make sure we are not accidentally increasing rope amp. 
+				repeat = int(float(offset)/loopTime * 10) - 1; // repeat once for every 0.1 of a looptime
+			} */
 			servoAmp = min(max( offset*ML_LOOP_OFFSET_TO_AMP + abs(ropeAngleOffset)*ML_ROPE_OFFSET_TO_AMP,0),maxServoAmp);
 		}
 
